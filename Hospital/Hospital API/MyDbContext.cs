728b3be8c5cba443e9ac9615a5f649569d0c64a1
vujsa7 <61715001+vujsa7@users.noreply.github.com>
2021-11-03 14:37:23 +0100

    Changed structure of project setup (#1)

---
+++ Hospital/Hospital API/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hospital_API.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
