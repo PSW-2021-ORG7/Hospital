9c25bc8b4efe5fd920fd113391b3c807dc7e0f9a
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2021-11-17 17:15:45 +0100

    ci: build and test ci

---
+++ Hospital/HospitalTests/Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HospitalTests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
