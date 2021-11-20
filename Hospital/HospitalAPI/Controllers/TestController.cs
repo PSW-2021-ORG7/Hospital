3231d07b24d9652bb5d75b7bb67e4dd01efc659d
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2021-11-20 01:37:27 +0100

    refactor: project update, TestController

---
+++ Hospital/HospitalAPI/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("It works!");
+        }
+    }
+}
