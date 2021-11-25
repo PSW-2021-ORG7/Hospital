4d8e194b17c60a596c3627c1f87ddf48255d1e97
Melanija Racic <racicmelanija@gmail.com>
2021-11-25 13:17:54 +0100

    feat: Reserve quantity in source room

--- Hospital/HospitalClassLibrary/RoomEquipment/Services/EquipmentTransferService.cs
+++ Hospital/HospitalClassLibrary/RoomEquipment/Services/EquipmentTransferService.cs
@@ -8,15 +8,21 @@ namespace HospitalClassLibrary.RoomEquipment.Services
     public class EquipmentTransferService : IEquipmentTransferService
     {
         private readonly IEquipmentTransferRepository _equipmentTransferRepository;
+        private readonly IEquipmentRepository _equipmentRepository;
 
-        public EquipmentTransferService(IEquipmentTransferRepository equipmentTransferRepository)
+        public EquipmentTransferService(IEquipmentTransferRepository equipmentTransferRepository, IEquipmentRepository equipmentRepository)
         {
             _equipmentTransferRepository = equipmentTransferRepository;
+            _equipmentRepository = equipmentRepository;
         }
 
         public async Task Create(EquipmentTransfer e)
         {
             await _equipmentTransferRepository.CreateAsync(e);
+
+            var equipment = await _equipmentRepository.GetByIdAsync(e.EquipmentId);
+            equipment.ReservedQuantity = e.Quantity;
+            await _equipmentRepository.UpdateAsync(equipment);
         }
     }
 }
