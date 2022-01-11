f069244f0c608a0aa2ddc6c54a65fa42a21a946e
SDragovic99 <73397006+SDragovic99@users.noreply.github.com>
2022-01-11 01:15:21 +0100

    fix: Delete workdays with shift

--- Hospital/HospitalClassLibrary/Schedule/Repositories/Interfaces/IWorkdayRepository.cs
+++ Hospital/HospitalClassLibrary/Schedule/Repositories/Interfaces/IWorkdayRepository.cs
@@ -11,5 +11,6 @@ namespace HospitalClassLibrary.Schedule.Repositories.Interfaces
         Task<IEnumerable<Workday>> GetWorkdays(DateTimeRange dateTimeRange, int doctorId);
         Task<IEnumerable<Appointment>> GetAppointments(DateTimeRange dateTimeRange, int srcRoomDoctorId, int dstRoomDoctorId);
         Task<IEnumerable<object>> GetAllShiftsByDoctorId(int doctorId);
+        Task<IEnumerable<Workday>> GetWorkdaysByShiftId(int shiftId);
     }
 }
