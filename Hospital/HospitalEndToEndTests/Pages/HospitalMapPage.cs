1b821ef2b9c9bb183254e0481fefe5e8236e008d
Melanija Racic <racicmelanija@gmail.com>
2022-01-25 00:32:54 +0100

    feat: Add hospital map page and tests

---
+++ Hospital/HospitalEndToEndTests/Pages/HospitalMapPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HospitalEndToEndTests.Pages
+{
+    public class HospitalMapPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly string _hospitalMapPageUri;
+        private IWebElement Building => _driver.FindElement(By.XPath("//*[name()='polygon' and contains(@id, 'building')]"));
+        
+
+        public HospitalMapPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _hospitalMapPageUri = "http://localhost:4200/hospital-map";
+        }
+
+        public void EnsureBuildingIsDisplayed()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return Building.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void ClickOnBuilding()
+        {
+            Building.Click();
+        }
+
+        public void Navigate() => _driver.Navigate().GoToUrl(_hospitalMapPageUri);
+    }
+}
