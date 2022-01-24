ec5c3373c700e420d116f4354529edd172822c55
Melanija Racic <racicmelanija@gmail.com>
2022-01-25 00:35:28 +0100

    feat: Add floor plan page and tests

---
+++ Hospital/HospitalEndToEndTests/FloorPlanTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HospitalEndToEndTests.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace HospitalEndToEndTests
+{
+    public class FloorPlanTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private readonly FloorPlanPage _floorPlanPage;
+
+        public FloorPlanTests()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+            options.AddArguments("disable-infobars");
+            options.AddArguments("--disable-extensions");
+            options.AddArguments("--disable-gpu");
+            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("--no-sandbox");
+            options.AddArguments("--disable-notifications");
+
+            _driver = new ChromeDriver(options);
+
+            _floorPlanPage = new FloorPlanPage(_driver);
+            _floorPlanPage.Navigate();
+            _floorPlanPage.EnsureFloorPlanIsDisplayed();
+            _floorPlanPage.EnsureRoomDetailsButtonIsDisabled();
+            _floorPlanPage.EnsureRoomsListIsNotEmpty();
+        }
+
+        [Fact]
+        public void Checks_if_correct_room_is_selected()
+        {
+            var selectedRoomName = _floorPlanPage.SelectRoom();
+            var selectedRoom = _floorPlanPage.FindSelectedRoom();
+            Assert.Equal(selectedRoomName, selectedRoom.Text);
+            Dispose();
+        }
+
+        [Fact]
+        public void Checks_if_room_details_btn_is_enabled()
+        {
+            _floorPlanPage.SelectRoom();
+            Assert.True(_floorPlanPage.IsRoomDetailsBtnEnabled());
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
---
+++ Hospital/HospitalEndToEndTests/Pages/FloorPlanPage.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HospitalEndToEndTests.Pages
+{
+    public class FloorPlanPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly string _floorPlanPageUri;
+        private IReadOnlyCollection<IWebElement> FloorPlan =>
+            _driver.FindElements(By.XPath("//*[name()='rect' and starts-with(@id, 'room-')]"));
+
+        private IWebElement RoomDetailsButton =>
+            _driver.FindElement(By.XPath("//*[@id='floor-plan']/app-side-bar/div/div[4]"));
+
+        private IReadOnlyCollection<IWebElement> RoomsList => _driver.FindElements(By.XPath("//*[contains(@id, 'card')]"));
+
+        public FloorPlanPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _floorPlanPageUri = "http://localhost:4200/hospital-map/floor-plan?buildingId=1";
+        }
+
+        public void EnsureFloorPlanIsDisplayed()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return FloorPlan.Count > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void EnsureRoomDetailsButtonIsDisabled()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return RoomDetailsButton.GetCssValue("cursor") == "default";
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public void EnsureRoomsListIsNotEmpty()
+        {
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    return RoomsList.Count > 0;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+        }
+
+        public string SelectRoom()
+        {
+            RoomsList.ElementAt(0).Click();
+            return RoomsList.ElementAt(0).FindElement(By.TagName("span")).Text;
+        }
+
+        public IWebElement FindSelectedRoom()
+        {
+            IWebElement selectedRoom = null;
+            var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 10));
+            wait.Until(condition =>
+            {
+                try
+                {
+                    selectedRoom = _driver.FindElement(By.XPath("//*[name()='text' and @style='fill: rgb(255, 255, 255); font-size: 14px; visibility: visible;']"));
+                    return selectedRoom.Displayed;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            });
+            return selectedRoom;
+        }
+
+        public bool IsRoomDetailsBtnEnabled()
+        {
+            return RoomDetailsButton.GetCssValue("cursor") == "pointer";
+        }
+
+        public void Navigate() => _driver.Navigate().GoToUrl(_floorPlanPageUri);
+    }
+}
